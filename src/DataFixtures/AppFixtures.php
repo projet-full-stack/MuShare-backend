c195d91a67e7a986d472b704de467ef7f16fed05
imercogo <remi.cogo@hotmail.fr>
2024-05-31 16:34:07 +0200

    add Song and Playlist fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,16 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Playlist;
 use App\Entity\Song;
-use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
-use Faker\Provider\en_US\Person;
 use Faker\Generator;
 
 class AppFixtures extends Fixture
 {
+
     private Generator $faker;
 
     public function __construct() {
@@ -19,16 +19,36 @@ class AppFixtures extends Fixture
     }
     public function load(ObjectManager $manager): void
     {
+        //Songs
+        $songs = [];
         for ($i=0; $i < 100; $i++) { 
-            // $song = new Song();
-            // $created = $this->faker->dateTime();
-            // $updated = $this->faker->dateTimeBetween($created, 'now');
-            // $song->setName($this->faker->word())->setStatus("on")->setCreatedAt($created)->setUpdatedAt($updated)->setAuthor($this->faker->name())->setPath($this->faker->imageUrl());
-            // $manager->persist($song);
-
-
+            $song = new Song();
+            $created = $this->faker->dateTime();
+            $updated = $this->faker->dateTimeBetween($created, 'now');
+            $song->setTitle($this->faker->word())->setStatus("on")->setCreatedAt($created)->setUpdatedAt($updated)->setAuthor($this->faker->name())->setPath($this->faker->imageUrl());
+            array_push($songs, $song);
+            $manager->persist($song);
         }
 
+        //Playlists
+        for ($i=0; $i < 100; $i++) { 
+            $playlist = new Playlist();
+            $created = $this->faker->dateTime();
+            $updated = $this->faker->dateTimeBetween($created, 'now');
+            shuffle($songs);
+            $fiveRandomElements = array_slice($songs, 0, 5);
+            $playlist->setTitle($this->faker->word())->setStatus("on")->setCreatedAt($created)->setUpdatedAt($updated)->addSong($fiveRandomElements[0])->addSong($fiveRandomElements[1])->addSong($fiveRandomElements[2])->addSong($fiveRandomElements[3])->addSong($fiveRandomElements[4]);
+            $manager->persist($playlist);
+        }
+        
+        //Likes
+        // for ($i=0; $i < 100; $i++) { 
+            // $like = new Like();
+            // $created = $this->faker->dateTime();
+            // $updated = $this->faker->dateTimeBetween($created, 'now');
+            // $like->setStatus("on")->setCreatedAt($created)->setUpdatedAt($updated);
+            // $manager->persist($like);
+        // }
 
         $manager->flush();
     }
--- src/Entity/Playlist.php
+++ src/Entity/Playlist.php
@@ -3,6 +3,9 @@
 namespace App\Entity;
 
 use App\Repository\PlaylistRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PlaylistRepository::class)]
@@ -15,6 +18,26 @@ class Playlist
 
     #[ORM\Column(length: 100)]
     private ?string $title = null;
+
+    #[ORM\Column(length: 25)]
+    private ?string $status = null;
+
+    #[ORM\Column(type: Types::DATETIMETZ_MUTABLE)]
+    private ?\DateTimeInterface $createdAt = null;
+
+    #[ORM\Column(type: Types::DATETIMETZ_MUTABLE)]
+    private ?\DateTimeInterface $updatedAt = null;
+
+    /**
+     * @var Collection<int, Song>
+     */
+    #[ORM\ManyToMany(targetEntity: Song::class)]
+    private Collection $songs;
+
+    public function __construct()
+    {
+        $this->songs = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +52,66 @@ class Playlist
     public function setTitle(string $title): static
     {
         $this->title = $title;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Song>
+     */
+    public function getSongs(): Collection
+    {
+        return $this->songs;
+    }
+
+    public function addSong(Song $song): static
+    {
+        if (!$this->songs->contains($song)) {
+            $this->songs->add($song);
+        }
+
+        return $this;
+    }
+
+    public function removeSong(Song $song): static
+    {
+        $this->songs->removeElement($song);
 
         return $this;
     }
