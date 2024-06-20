dc581078f7301e2cfe286047b4152a6eb35fe5c9
imercogo <remi.cogo@hotmail.fr>
2024-06-20 16:50:16 +0200

    feat: add fixtures for users and update others

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Follow;
+use App\Entity\Like;
 use App\Entity\Playlist;
 use App\Entity\Song;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -19,36 +22,63 @@ class AppFixtures extends Fixture
     }
     public function load(ObjectManager $manager): void
     {
+        //Users
+        $users = [];
+        for ($i=0; $i < 10; $i++) {
+            $user = new User();
+            $created = $this->faker->dateTime();
+            $updated = $this->faker->dateTimeBetween($created, 'now');
+            $user->setUsername($this->faker->userName())->setEmail($this->faker->email())->setPassword($this->faker->password())->setCreatedAt($created)->setUpdatedAt($updated)->setStatus("on");
+            array_push($users, $user);
+            $manager->persist($user);
+        }
+
         //Songs
         $songs = [];
         for ($i=0; $i < 100; $i++) { 
             $song = new Song();
             $created = $this->faker->dateTime();
             $updated = $this->faker->dateTimeBetween($created, 'now');
+            $song->setUser($users[array_rand($users)]);
             $song->setTitle($this->faker->word())->setStatus("on")->setCreatedAt($created)->setUpdatedAt($updated)->setAuthor($this->faker->name())->setPath($this->faker->imageUrl());
             array_push($songs, $song);
             $manager->persist($song);
         }
 
         //Playlists
+        $playlists = [];
         for ($i=0; $i < 100; $i++) { 
             $playlist = new Playlist();
             $created = $this->faker->dateTime();
             $updated = $this->faker->dateTimeBetween($created, 'now');
             shuffle($songs);
             $fiveRandomElements = array_slice($songs, 0, 5);
+            $playlist->setOwner($users[array_rand($users)]);
             $playlist->setTitle($this->faker->word())->setStatus("on")->setCreatedAt($created)->setUpdatedAt($updated)->addSong($fiveRandomElements[0])->addSong($fiveRandomElements[1])->addSong($fiveRandomElements[2])->addSong($fiveRandomElements[3])->addSong($fiveRandomElements[4]);
+            array_push($playlists, $playlist);
             $manager->persist($playlist);
         }
         
         //Likes
-        // for ($i=0; $i < 100; $i++) { 
-            // $like = new Like();
-            // $created = $this->faker->dateTime();
-            // $updated = $this->faker->dateTimeBetween($created, 'now');
-            // $like->setStatus("on")->setCreatedAt($created)->setUpdatedAt($updated);
-            // $manager->persist($like);
-        // }
+        for ($i=0; $i < 100; $i++) { 
+            $like = new Like();
+            $created = $this->faker->dateTime();
+            $updated = $this->faker->dateTimeBetween($created, 'now');
+            $like->setUser($users[array_rand($users)])->setSong($songs[array_rand($songs)]);
+            $like->setStatus("on")->setCreatedAt($created)->setUpdatedAt($updated);
+            $manager->persist($like);
+        }
+        //Follows
+        for ($i=0; $i<50; $i++) {
+            $user = $users[array_rand($users)];
+            $follow = new Follow();
+            $follow->setUser($user);
+            $follow->setPlaylist($playlists[array_rand($playlists)]);
+            $created = $this->faker->dateTime();
+            $updated = $this->faker->dateTimeBetween($created, 'now');
+            $follow->setCreatedAt($created)->setUpdatedAt($updated)->setStatus("on");
+            $manager->persist($follow);
+        }
 
         $manager->flush();
     }
--- src/Entity/Follow.php
+++ src/Entity/Follow.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\FollowRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 
@@ -25,6 +26,15 @@ class Follow
      * @Groups({"follow"})
      */
     private ?User $user = null;
+
+    #[ORM\Column(type: Types::DATETIMETZ_MUTABLE)]
+    private ?\DateTimeInterface $updatedAt = null;
+
+    #[ORM\Column(type: Types::DATETIMETZ_MUTABLE)]
+    private ?\DateTimeInterface $createdAt = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $status = null;
 
     public function getId(): ?int
     {
@@ -51,6 +61,42 @@ class Follow
     public function setUser(?User $user): static
     {
         $this->user = $user;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
 
         return $this;
     }
